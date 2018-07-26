60ed3cb0414b068373cf22aff50d60f0730aa841
Jaime Wissner <jaime.wissner@gmail.com>
2018-07-26 14:30:05 -0400

    Added snapshots

--- tests/Home.test.js
+++ tests/Home.test.js
@@ -1,8 +1,14 @@
 import React from "react"
 import {Home} from "../src/components/Home"
 import { shallow } from "enzyme"
+import renderer from 'react-test-renderer'
 
 describe("Home component", () => {
+    it("should match the snapshot", () => {
+        let tree = renderer.create(<Home />).toJSON();
+        expect(tree).toMatchSnapshot();
+    })
+
     it("says name of app", () => {
         const wrapper = shallow(<Home />);
         let text = wrapper.find("h2").text();
